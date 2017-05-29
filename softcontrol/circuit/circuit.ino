96da0205c028a86308cd7b3b4d0685dad4c22485
Avnerus <avnerus@gmail.com>
2017-05-29 19:15:44 +0900

    Arduino circuit

---
+++ softcontrol/circuit/circuit.ino
@@ -0,0 +1,30 @@
+int E1 = 6;
+int M1 = 7;
+int V1 = 9;
+
+void setup() {
+  pinMode(E1, OUTPUT);
+  pinMode(V1, OUTPUT);
+  Serial.begin(9600); 
+  int sensorValue = analogRead(A0);
+}
+
+void loop() {
+    int sensorValue = analogRead(A0);
+    int motorSpeed = round((float)sensorValue / 1023 * 255);
+    //Serial.println(motorSpeed);   
+    digitalWrite(M1,HIGH);
+    analogWrite(E1, motorSpeed);
+
+
+    // Protection
+    int pressure = analogRead(A1);
+    if (pressure > 600) {
+     digitalWrite(V1, HIGH);
+    } else if (pressure < 200) {
+      digitalWrite(V1, LOW);
+    }
+    Serial.println(pressure); 
+    
+    delay(30);
+}
