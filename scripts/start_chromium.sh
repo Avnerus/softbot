84617f368ac39ba0d1b15f33b8874cb22de302a6
Avnerus <avnerus@gmail.com>
2017-07-06 12:13:36 +0900

    script update

--- scripts/start_chromium.sh
+++ scripts/start_chromium.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/chromium-browser --enable-speech-dispatcher --disable-web-security --disable-session-crashed-bubble --disable-infobars
+/usr/bin/chromium-browser --enable-speech-dispatcher --disable-web-security --user-data-dir --disable-session-crashed-bubble --disable-infobars
