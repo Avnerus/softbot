f4912e4fef0857f909d3c11d2810d6a2c60ffc3f
Avnerus <avnerus@gmail.com>
2017-07-08 02:20:12 +0900

    USB Port argument for c++

--- softcontrol/softcontrol.cpp
+++ softcontrol/softcontrol.cpp
@@ -13,19 +13,23 @@ void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
         serial.write_some(msg->get_payload());
 }
 
-int main() {
-    server print_server;
-    bool rv = serial.start("/dev/ttyACM0", 9600);
-    if (rv == false) {
-                return -1;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <Arduino USB device>" << std::endl;
+    } else {
+        server print_server;
+        bool rv = serial.start(argv[1], 9600);
+        if (rv == false) {
+            std::cout << "Failed to connect to serial port. Exiting." << std::endl;
+            return -1;
+        }   
 
-    }   
+        print_server.set_message_handler(&on_message);
 
-    print_server.set_message_handler(&on_message);
+        print_server.init_asio();
+        print_server.listen(9002);
+        print_server.start_accept();
 
-    print_server.init_asio();
-    print_server.listen(9002);
-    print_server.start_accept();
-
-    print_server.run();
+        print_server.run();
+    }
 }
