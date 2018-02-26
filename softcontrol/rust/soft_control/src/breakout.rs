7c7c7a042856c9208e61b9711c02870918f961c7
Avnerus <avnerus@gmail.com>
2018-02-26 23:34:36 +0200

    added game loop thread

--- softcontrol/rust/soft_control/src/breakout.rs
+++ softcontrol/rust/soft_control/src/breakout.rs
@@ -1,10 +1,10 @@
-use std::thread;
+use std::{thread,time};
 use std::time::Duration;
-use std::sync::{Arc};
+use std::sync::{Arc,Mutex};
 use std::sync::mpsc;
 use Config;
 
-struct Game {
+struct GameState {
     tick: u32
 }
 
@@ -12,15 +12,25 @@ pub fn start(
     config: Arc<Config>,
     breakout_rx: mpsc::Receiver<Vec<u8>>
 ) {
-    let mut game = Game {
+    let mut game_state = Arc::new(Mutex::new(GameState {
         tick: 0
-    };
+    }));
     println!("Breakout game starting {:?}",config);
-    /*
-    loop {
-        game.tick += 1
-    }*/
+    let game_loop = 
+        thread::Builder::new().name("game_loop".to_owned()).spawn(move || {
+            game_loop(game_state.clone());
+    }).unwrap();
     while let Ok(msg) = breakout_rx.recv() {
         println!("Breakout msg! {:?}", msg);
+    }
+
+}
+
+fn game_loop(game_state: Arc<Mutex<GameState>>) {
+    loop {
+        let mut game_state = game_state.lock().unwrap();
+        game_state.tick += 1;
+        println!("Tick");
+        thread::sleep(time::Duration::from_millis(1000));
     }
 }
