a3943198a5a8e827af0b714082aec074ebf66f4c
Avnerus <avnerus@gmail.com>
2018-05-28 00:02:00 +0300

    Working on Breakout Rust/WASM module. Moving to Webpack

--- softcontrol/rust/soft_control/src/breakout.rs
+++ softcontrol/rust/soft_control/src/breakout.rs
@@ -1,36 +1,19 @@
-use std::{thread,time};
-use std::time::Duration;
-use std::sync::{Arc,Mutex};
-use std::sync::mpsc;
-use Config;
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
 
-struct GameState {
-    tick: u32
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
 }
 
-pub fn start(
-    config: Arc<Config>,
-    breakout_rx: mpsc::Receiver<Vec<u8>>
-) {
-    let mut game_state = Arc::new(Mutex::new(GameState {
-        tick: 0
-    }));
-    println!("Breakout game starting {:?}",config);
-    let game_loop = 
-        thread::Builder::new().name("game_loop".to_owned()).spawn(move || {
-            game_loop(game_state.clone());
-    }).unwrap();
-    while let Ok(msg) = breakout_rx.recv() {
-        println!("Breakout msg! {:?}", msg);
-    }
-
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
 
-fn game_loop(game_state: Arc<Mutex<GameState>>) {
-    loop {
-        let mut game_state = game_state.lock().unwrap();
-        game_state.tick += 1;
-        println!("Tick");
-        thread::sleep(time::Duration::from_millis(1000));
-    }
+pub fn step() {
+    println!("Breakout-Step");
 }
