5a64767013d91b9e97642e0bbc6dc77978dd047c
avnerus <avnerus@gmail.com>
2019-03-01 23:42:05 +0200

    some better valve test functions

--- softcontrol/test_valve/test_valve.ino
+++ softcontrol/test_valve/test_valve.ino
@@ -1,23 +1,24 @@
 #include "valve.h"
 #include "pumpng.h"
-
 /*
-   Right neck
+   Left neck 
+*/
 Valve* release = new Valve(2,3,5,25,1023);
 Valve* entry   = new Valve(2,26,5,25,1023);
-*/
 
-/*
-   Left neck
+/* Right neck  
 Valve* release = new Valve(6,7,9,25, 1023);
 Valve* entry  = new Valve(6,8,9,25, 1023);
 */
 
- /* Right Arm   */
+
+ /* Right Arm   
 Valve* entry  = new Valve(17,15,37,25);
 Valve* release = new Valve(17,16,37,25);
+*/
 
-/* Left Arm 
+
+/* Left Arm  
 Valve* entry  = new Valve(20,18,38,25);
 Valve* release = new Valve(20,19,38,25);
 */
@@ -37,11 +38,11 @@ void setup() {
   entry->init();
   release->init();
   pump->init();
-  pump->setSpeed(0.5);
+  pump->setSpeed(0.0);
   Serial.println("Deflate completely");
 
-  entry->close();
-  release->open();
+  //entry->close();
+  //release->open();
   delay(2000);
 
   clock = millis();
@@ -50,6 +51,30 @@ void setup() {
 
 void loop() {
     int timer = millis() - clock;
+    //inflateDeflate(timer);
+    openClose(timer);
+}
+
+void openClose(int timer) {
+    if (timer >= 0 && stage == 0) {
+        Serial.println("Open");
+        release->open();
+        entry->open();
+        stage = 1;
+    }
+    else if (timer >= 2000 && stage == 1) {
+        Serial.println("close");
+        entry->close();
+        release->close();
+        stage = 2;
+    }
+    else if (timer >= 4000 && stage == 2) {
+        clock = millis();
+        stage = 0;
+    }
+}
+
+void inflateDeflate(int timer) {
     if (timer >= 0 && stage == 0) {
         Serial.println("Inflate");
         release->close();
